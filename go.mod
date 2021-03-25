d2dafdd36a591ede074ab09136e4ed925f4c729e
ameya <ameyarao98@gmail.com>
2021-03-25 12:35:51 +0100

    more setup

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module gitlab.com/AmeyaRao98/glassnode-ethereum-fee
 
 go 1.16
 
-require github.com/lib/pq v1.10.0 // indirect
+require (
+	github.com/jackc/pgx v3.6.2+incompatible // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2 // indirect
+	golang.org/x/text v0.3.5 // indirect
+)
--- main.go
+++ main.go
@@ -2,10 +2,33 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+
+	"github.com/jackc/pgx"
 )
 
+type eoaFees struct {
+	Timestamp int64 `json:"t"`
+	Fees      int32 `json:"v"`
+}
+
 func main() {
+	pgxConfig := pgx.ConnConfig{
+		Host:     "database",
+		Port:     5432,
+		Database: os.Getenv("POSTGRES_DB"),
+		User:     os.Getenv("POSTGRES_USER"),
+		Password: os.Getenv("POSTGRES_PASSWORD"),
+	}
+	pgxConnPoolConfig := pgx.ConnPoolConfig{ConnConfig: pgxConfig}
+	_, err := pgx.NewConnPool(pgxConnPoolConfig)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		log.Printf("Connected to postgres")
+	}
 	http.HandleFunc("/", HelloServer)
 	http.ListenAndServe(":8080", nil)
 }
